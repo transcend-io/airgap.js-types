d67b32b28e0dfb5f87a4c64863bd44b7945bc7af
anotherminh <minh@transcend.io>
2021-12-14 23:47:48 -0500

    Remove unused deps

--- .pnp.cjs
+++ .pnp.cjs
@@ -34,13 +34,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./",
           "packageDependencies": [
             ["@transcend-io/type-utils", "npm:1.0.4"],
-            ["@types/chai", "npm:4.3.0"],
             ["@types/node", "npm:16.11.13"],
             ["@typescript-eslint/eslint-plugin", "virtual:d262a2542c3ed3ed86c11b9ff398f7194c0f4f174cdc58addabf550f1f32ecdd3b75fbb2147b0315aab4620df498ccccbb7a830e24e6351590360415ce86dd82#npm:5.7.0"],
             ["@typescript-eslint/parser", "virtual:d262a2542c3ed3ed86c11b9ff398f7194c0f4f174cdc58addabf550f1f32ecdd3b75fbb2147b0315aab4620df498ccccbb7a830e24e6351590360415ce86dd82#npm:5.7.0"],
             ["@yarnpkg/pnpify", "npm:3.1.0"],
             ["@yarnpkg/sdks", "npm:2.5.0"],
-            ["chai", "npm:4.3.4"],
             ["depcheck", "npm:1.4.2"],
             ["eslint", "npm:7.32.0"],
             ["eslint-config-airbnb-base", "virtual:d262a2542c3ed3ed86c11b9ff398f7194c0f4f174cdc58addabf550f1f32ecdd3b75fbb2147b0315aab4620df498ccccbb7a830e24e6351590360415ce86dd82#npm:14.2.1"],
@@ -406,13 +404,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["@transcend-io/airgap.js-types", "workspace:."],
             ["@transcend-io/type-utils", "npm:1.0.4"],
-            ["@types/chai", "npm:4.3.0"],
             ["@types/node", "npm:16.11.13"],
             ["@typescript-eslint/eslint-plugin", "virtual:d262a2542c3ed3ed86c11b9ff398f7194c0f4f174cdc58addabf550f1f32ecdd3b75fbb2147b0315aab4620df498ccccbb7a830e24e6351590360415ce86dd82#npm:5.7.0"],
             ["@typescript-eslint/parser", "virtual:d262a2542c3ed3ed86c11b9ff398f7194c0f4f174cdc58addabf550f1f32ecdd3b75fbb2147b0315aab4620df498ccccbb7a830e24e6351590360415ce86dd82#npm:5.7.0"],
             ["@yarnpkg/pnpify", "npm:3.1.0"],
             ["@yarnpkg/sdks", "npm:2.5.0"],
-            ["chai", "npm:4.3.4"],
             ["depcheck", "npm:1.4.2"],
             ["eslint", "npm:7.32.0"],
             ["eslint-config-airbnb-base", "virtual:d262a2542c3ed3ed86c11b9ff398f7194c0f4f174cdc58addabf550f1f32ecdd3b75fbb2147b0315aab4620df498ccccbb7a830e24e6351590360415ce86dd82#npm:14.2.1"],
@@ -485,15 +481,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/keyv", "npm:3.1.3"],
             ["@types/node", "npm:16.11.13"],
             ["@types/responselike", "npm:1.0.0"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["@types/chai", [
-        ["npm:4.3.0", {
-          "packageLocation": "./.yarn/cache/@types-chai-npm-4.3.0-42bcda9bbc-3e393e0942.zip/node_modules/@types/chai/",
-          "packageDependencies": [
-            ["@types/chai", "npm:4.3.0"]
           ],
           "linkType": "HARD",
         }]
@@ -1289,15 +1276,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["assertion-error", [
-        ["npm:1.1.0", {
-          "packageLocation": "./.yarn/cache/assertion-error-npm-1.1.0-66b893015e-fd9429d3a3.zip/node_modules/assertion-error/",
-          "packageDependencies": [
-            ["assertion-error", "npm:1.1.0"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["astral-regex", [
         ["npm:2.0.0", {
           "packageLocation": "./.yarn/cache/astral-regex-npm-2.0.0-f30d866aab-876231688c.zip/node_modules/astral-regex/",
@@ -1434,21 +1412,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["chai", [
-        ["npm:4.3.4", {
-          "packageLocation": "./.yarn/cache/chai-npm-4.3.4-808f3b5355-772c522b3b.zip/node_modules/chai/",
-          "packageDependencies": [
-            ["chai", "npm:4.3.4"],
-            ["assertion-error", "npm:1.1.0"],
-            ["check-error", "npm:1.0.2"],
-            ["deep-eql", "npm:3.0.1"],
-            ["get-func-name", "npm:2.0.0"],
-            ["pathval", "npm:1.1.1"],
-            ["type-detect", "npm:4.0.8"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["chalk", [
         ["npm:2.4.2", {
           "packageLocation": "./.yarn/cache/chalk-npm-2.4.2-3ea16dd91e-ec3661d38f.zip/node_modules/chalk/",
@@ -1475,15 +1438,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["chalk", "npm:4.1.2"],
             ["ansi-styles", "npm:4.3.0"],
             ["supports-color", "npm:7.2.0"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["check-error", [
-        ["npm:1.0.2", {
-          "packageLocation": "./.yarn/cache/check-error-npm-1.0.2-00c540c6e9-d9d1065044.zip/node_modules/check-error/",
-          "packageDependencies": [
-            ["check-error", "npm:1.0.2"]
           ],
           "linkType": "HARD",
         }]
@@ -1794,16 +1748,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["decompress-response", "npm:6.0.0"],
             ["mimic-response", "npm:3.1.0"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["deep-eql", [
-        ["npm:3.0.1", {
-          "packageLocation": "./.yarn/cache/deep-eql-npm-3.0.1-9a66c09c65-4f4c9fb79e.zip/node_modules/deep-eql/",
-          "packageDependencies": [
-            ["deep-eql", "npm:3.0.1"],
-            ["type-detect", "npm:4.0.8"]
           ],
           "linkType": "HARD",
         }]
@@ -2653,15 +2597,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/get-caller-file-npm-2.0.5-80e8a86305-b9769a836d.zip/node_modules/get-caller-file/",
           "packageDependencies": [
             ["get-caller-file", "npm:2.0.5"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["get-func-name", [
-        ["npm:2.0.0", {
-          "packageLocation": "./.yarn/cache/get-func-name-npm-2.0.0-afbf363765-8d82e69f3e.zip/node_modules/get-func-name/",
-          "packageDependencies": [
-            ["get-func-name", "npm:2.0.0"]
           ],
           "linkType": "HARD",
         }]
@@ -4058,15 +3993,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["pathval", [
-        ["npm:1.1.1", {
-          "packageLocation": "./.yarn/cache/pathval-npm-1.1.1-ce0311d7e0-090e314771.zip/node_modules/pathval/",
-          "packageDependencies": [
-            ["pathval", "npm:1.1.1"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["picocolors", [
         ["npm:1.0.0", {
           "packageLocation": "./.yarn/cache/picocolors-npm-1.0.0-d81e0b1927-a2e8092dd8.zip/node_modules/picocolors/",
@@ -5014,15 +4940,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["type-check", "npm:0.4.0"],
             ["prelude-ls", "npm:1.2.1"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["type-detect", [
-        ["npm:4.0.8", {
-          "packageLocation": "./.yarn/cache/type-detect-npm-4.0.8-8d8127b901-62b5628bff.zip/node_modules/type-detect/",
-          "packageDependencies": [
-            ["type-detect", "npm:4.0.8"]
           ],
           "linkType": "HARD",
         }]
