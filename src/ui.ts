d0275a309163493246793a0241472713c1582a66
anotherminh <minhngoc.ln@gmail.com>
2023-11-20 14:10:26 -0500

    remove unused import

--- src/ui.ts
+++ src/ui.ts
@@ -4,7 +4,6 @@ import * as t from 'io-ts';
 // main
 import {
   applyEnum,
-  FixedLengthArray,
   valuesOf,
 } from '@transcend-io/type-utils';
 import { ConsentManagerLanguageKey } from '@transcend-io/internationalization';
@@ -252,21 +251,21 @@ export type TranscendView = Window & {
 };
 
 export const DEFAULT_VIEW_STATE_BY_PRIVACY_REGIME: ExperienceToInitialViewState =
-  {
-    // EU
-    GDPR: InitialViewState.QuickOptions,
-    // Brazil
-    LGPD: InitialViewState.QuickOptions,
-    // Switzerland
-    nFADP: InitialViewState.QuickOptions,
-    // US: California
-    CPRA: InitialViewState.Hidden,
-    // US: Virginia
-    CDPA: InitialViewState.Hidden,
-    // US: Colorado
-    CPA: InitialViewState.Hidden,
-    // US: Nevada
-    NEVADA_SB220: InitialViewState.Hidden,
-    // Other
-    Unknown: InitialViewState.Hidden,
-  };
+{
+  // EU
+  GDPR: InitialViewState.QuickOptions,
+  // Brazil
+  LGPD: InitialViewState.QuickOptions,
+  // Switzerland
+  nFADP: InitialViewState.QuickOptions,
+  // US: California
+  CPRA: InitialViewState.Hidden,
+  // US: Virginia
+  CDPA: InitialViewState.Hidden,
+  // US: Colorado
+  CPA: InitialViewState.Hidden,
+  // US: Nevada
+  NEVADA_SB220: InitialViewState.Hidden,
+  // Other
+  Unknown: InitialViewState.Hidden,
+};
