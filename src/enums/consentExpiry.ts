16fe222f8d8a997ef28e20d0845827cb4d93c1c4
Cami <50341430+csmccarthy@users.noreply.github.com>
2023-12-04 12:36:40 -0600

    add enum for behavior on consent expiry (#120)

---
+++ src/enums/consentExpiry.ts
@@ -0,0 +1,16 @@
+import { makeEnum } from '@transcend-io/type-utils';
+
+/**
+ * Enum of options available for behavior to exhibit when the user's consent has expired
+ */
+export const OnConsentExpiry = makeEnum({
+  /** Reprompts the user to change or confirm their selection */
+  Prompt: 'prompt',
+  /** Resets the user's consent and reprompts them */
+  Reset: 'reset',
+  /** Resets the user's opted-in purposes and reprompts them */
+  ResetOptIns: 'reset-opt-ins',
+});
+/** Override type */
+export type OnConsentExpiry =
+  typeof OnConsentExpiry[keyof typeof OnConsentExpiry];
