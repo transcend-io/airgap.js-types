caf982bbfe10f2a4bdf1b64f4eba69d4d4679612
anotherminh <minhngoc.ln@gmail.com>
2024-05-01 11:44:55 -0400

    Add setting for GCM integration with TCF

--- src/ui.ts
+++ src/ui.ts
@@ -235,6 +235,11 @@ export const TCFBundledDataConfig = t.partial({
   languages: t.string,
   /** Default locale to use for the UI internationalization  */
   locale: t.string,
+  /**
+   * Whether or not to enable integration between Google Consent Mode and TCF:
+   * https://developers.google.com/tag-platform/security/guides/implement-TCF-strings#cmp-api
+   */
+  enableAdvertiserConsentMode: t.boolean
 });
 
 /** Type override */
@@ -255,21 +260,21 @@ export type TranscendView = Window & {
 };
 
 export const DEFAULT_VIEW_STATE_BY_PRIVACY_REGIME: ExperienceToInitialViewState =
-  {
-    // EU
-    GDPR: InitialViewState.QuickOptions,
-    // Brazil
-    LGPD: InitialViewState.QuickOptions,
-    // Switzerland
-    nFADP: InitialViewState.QuickOptions,
-    // US: California
-    CPRA: InitialViewState.Hidden,
-    // US: Virginia
-    CDPA: InitialViewState.Hidden,
-    // US: Colorado
-    CPA: InitialViewState.Hidden,
-    // US: Nevada
-    NEVADA_SB220: InitialViewState.Hidden,
-    // Other
-    Unknown: InitialViewState.Hidden,
-  };
+{
+  // EU
+  GDPR: InitialViewState.QuickOptions,
+  // Brazil
+  LGPD: InitialViewState.QuickOptions,
+  // Switzerland
+  nFADP: InitialViewState.QuickOptions,
+  // US: California
+  CPRA: InitialViewState.Hidden,
+  // US: Virginia
+  CDPA: InitialViewState.Hidden,
+  // US: Colorado
+  CPA: InitialViewState.Hidden,
+  // US: Nevada
+  NEVADA_SB220: InitialViewState.Hidden,
+  // Other
+  Unknown: InitialViewState.Hidden,
+};
